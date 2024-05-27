58127610784acd7fcba23b1014baa219dbedca90
Lockstar14 <pomincukr@gmail.com>
2024-05-27 14:31:37 +0500

    lesson 2 task 6 first commit

---
+++ lesson-02/06.js
@@ -0,0 +1,19 @@
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+
+let passportWithAddress1 = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    }
+};
+console.log(passportWithAddress.city)
+console.log(passportWithAddress1.city)
