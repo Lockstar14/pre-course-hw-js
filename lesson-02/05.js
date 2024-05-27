10c9bc49df3d1a295521c66170617abecc070307
Lockstar14 <pomincukr@gmail.com>
2024-05-27 13:59:59 +0500

    lesson 2 task 5 first commit

---
+++ lesson-02/05.js
@@ -0,0 +1,12 @@
+let passport = {
+    name: "Petr",
+    surname: "Petrov",
+};
+
+let passport1 ={
+    name: "Ivan",
+    surname: "Petrov",
+}
+
+console.log(passport.name)
+console.log(passport1.name)
